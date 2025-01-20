fc9712e995efac86f7c87b24425c6872eac06e0d
Bulbul Ahammed <135580438+bulbulapex@users.noreply.github.com>
2025-01-20 21:33:11 +0600

    try db coonection

---
+++ src/main/java/org/example/neovault/UserRepo.java
@@ -0,0 +1,18 @@
+package org.example.neovault;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepo extends JpaRepository<Users, Long> {
+
+    // Custom query to find users by their name
+
+    @Query("SELECT u FROM tbl_users u WHERE u.name = :name")
+    List<Users> findByName(@Param("name") String name);
+
+
+}
